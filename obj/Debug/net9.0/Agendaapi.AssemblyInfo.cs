e552a97fbb44fdbfec8724e787a47fa89b02ee96
Everaldo arruda <dev.ejmarf@gmail.com>
2024-12-23 19:47:46 -0300

    resolução

--- obj/Debug/net9.0/Agendaapi.AssemblyInfo.cs
+++ obj/Debug/net9.0/Agendaapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agendaapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c3be98e7e0bcfcfc8ad51122c9ee70b9413a64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agendaapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agendaapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
